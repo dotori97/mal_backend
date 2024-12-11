4b47f5291319a963f47dce0f42b2a472eeaa6eb8
migyeong <obbddo97@gmail.com>
2024-12-11 13:25:57 +0900

    initial commit

---
+++ config/config.js
@@ -0,0 +1,36 @@
+// require('dotenv').config();
+require('dotenv').config();
+
+console.log('Environment Variables:', {
+  MY_DB_USERNAME: process.env.MY_DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST,
+  DB_DIALECT: process.env.DB_DIALECT,
+});
+
+const config = {
+  development: {
+    username: process.env.MY_DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.MY_DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.MY_DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
+
+module.exports = config;
